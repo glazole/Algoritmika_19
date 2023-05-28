d12030fc658b2a5d39b062ffa3c519ca5e301dd9
glazole <glazkov-oleg@yandex.ru>
2023-05-28 22:15:40 +0300

    fix changes

--- leader_19/recomendation/app/forms.py
+++ leader_19/recomendation/app/forms.py
@@ -39,32 +39,4 @@ class AnswerForm(forms.Form):
         return profile
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=64)
-    password = forms.CharField(widget = forms.PasswordInput)
-
-#    def clean_username(self):
- #       username = self.cleaned_data['username']
-  #      if username == None or username.strip() == "":
-   #         raise forms.ValidationError("Ник не должен содержать пробелы")
-    #    return username.strip()
-
-   # def clean_password(self):
-    ##    password = self.cleaned_data['password']
-      #  if password == None or password.strip() == "":
-       #     raise forms.ValidationError("Empty field Password")
-       # return password.strip()
-
-    def clean(self, *args, **kwargs):
-        username = self.cleaned_data['username']
-        password = self.cleaned_data['password']
-        try:            
-            user = UserId.objects.get(username=username)
-        except User.DoesNotExist:
-            raise forms.ValidationError('Такого пользователя не существует')  
-        if user and not user.check_password(password):
-            raise forms.ValidationError("Incorrect password!")
-        if user and not user.is_active:
-            raise forms.ValidationError('This user is no longer active.')
-        return super(LoginForm, self).clean(*args, **kwargs)
-
+ 
--- leader_19/recomendation/app/views.py
+++ leader_19/recomendation/app/views.py
@@ -36,7 +36,7 @@ def results(request):
 def vote(request, id):
     question_list = Question.objects.all()
     for question in question_list:
-   # question = get_object_or_404(Question, pk=question.id)
+
         try:
             selected_choice = question.choice_set.get(pk=request.POST['choice'])
         except (KeyError, Choice.DoesNotExist): 
@@ -47,12 +47,12 @@ def vote(request, id):
         else:
             selected_choice.votes += int (request.GET['votes'])
             selected_choice.save()
-    return HttpResponseRedirect(reverse('app:results')) #, args=(id,)))
+    return HttpResponseRedirect(reverse('app:results'))
+
 
 def test(request):
     choice = Choice.objects.all()
     question_list = Question.objects.all()
-    #question_list = Question.objects.get_queryset().order_by('id')
     paginator = Paginator(question_list, 1)
     page = request.GET.get('page')
 
@@ -63,66 +63,3 @@ def test(request):
     except EmptyPage:
         question = paginator.page(paginator.num_pages)
     return render(request, 'app/test.html', {'question': question, 'page': page, 'choice': choice})
-
-    # if paginator.page(page) == 11:
-   #     return HttpResponse ('votes') 
-
-def test3(request):
-    data={'form_':''}
-    #   Если в сессии нет нужных переменных, то создаем
-    if 'question' not in request.session:
-        request.session['question']={}
-        request.session['vote'] = 0
-        index = 1
-        for row in my_quests:
-            request.session['question'][index]=None
-            index +=1
-        request.session.modified = True
-
-    #   нам пришел ответ, то считаем его
-    if 'vote' in request.GET:
-        request.session['vote'] += int (request.GET['vote'])
-        request.session['question'][request.GET['page']] = 'Done'
-        request.session.modified = True
-        print (request.session['vote'])
-
-    if 'page' not in request.GET:
-        return redirect ('/test/?page=1')
-    else:
-        #   Если был получен ответ на этот вопрос, то идем дальше
-        if request.session['question'][request.GET['page']] != None:
-            next_page = int(request.GET['page'])+1
-            #   Если на все вопросы даны ответы, то возвращаем результат
-            if next_page-1>=len(my_quests):
-                return HttpResponse ('Вы завершили тестирование и набрали '+str(request.session['vote'])+' баллов!')
-            return redirect ('/test/?page='+ str(next_page))
-        else:
-            #   Рендерим форму
-            data['form_']=render_form (int(request.GET['page']))
-    return render (request, 'index.html', data)
-
-
-#    return render(request, 'app/test.html', {
- #   'question': question, 'page': page, 'choice': choice})
-
-
-#   Переменная с вопросами и ответами/баллами
-my_quests = {
-    1:['Выберите любое число', ['Два', 0],['Семь', 1],['Восемь', 2]],
-    2:['Выберите любой цвет', ['Красный', 0],['Желтый', 1],['Синий', 2]],
-    3:['Выберите любое животное', ['Корова', 0],['Носорог', 1],['Жираф', 2]],
-    4:['Выберите то, что нельзя выбрать', ['Можно выбрать', 0],['Нельзя выбрать', 1]],
-    5:['Попробуйте ничего не выбрать', ['Выбрать', 0],['Не выбрать', 1]],
-}
-
-#   Отвечает за формирование элементов в форме
-def render_form (number):
-    choices = Choice.objects.all()
-    questions = Question.objects.all()
-
-    HTML_='<input name="page" value="'+str(number)+'" hidden><h3>'+ questsions[number]+'</h3>'
-    for row in questions[number][1:len(queststions[number])]:
-        HTML_+='<p>'+row[0]+' <input name="vote" value="'+str(row[1])+'" type="radio"></p>'
-    return HTML_
-
-
